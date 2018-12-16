542d2073a6dbdfbd4069fb779f44c971cd2c202d
mbishop8 <mbishop8@myune.edu.au>
2018-12-16 18:51:18 +1100

    Added validation logic

--- AbnTools/AbnValidator.cs
+++ AbnTools/AbnValidator.cs
@@ -1,10 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AbnTools
 {
-    class AbnValidator
+    public static class AbnValidator
     {
+        private static Dictionary<int, int> positionWeights = new Dictionary<int, int>()
+        {
+            {0, 10 },
+            {1, 1 },
+            {2, 3 },
+            {3, 5 },
+            {4, 7 },
+            {5, 9 },
+            {6, 11 },
+            {7, 13 },
+            {8, 15 },
+            {9, 17 },
+            {10, 19 },
+        };
+
+
+        /// <summary>
+        /// Checks that an Australian Busness Number (ABN) is valid, according 
+        /// to the Australian Business Register's check algorithm
+        /// See: https://abr.business.gov.au/Help/AbnFormat
+        /// </summary>
+        /// <param name="abn">ABN as string</param>
+        /// <returns>True if the ABN is valid, otherwse returns false.</returns>
+        public static bool Validate(string abn)
+        {
+            // remove any spaces
+            abn = abn.Replace(" ", "");
+
+            // ABN's must be 11 numerals
+            if (!Regex.IsMatch(abn, @"[0-9]{11}")) { return false; }
+
+            // split into 11 integers
+            var digits = abn.Select(d => int.Parse(d.ToString())).ToList();
+
+            digits[0] = digits[0] - 1; // subtract 1 from first digit
+
+            var weightedDigits = new List<int>();
+            // multiple each digit by the prescribed weighting factor
+            for (int i = 0; i < digits.Count; i++)
+            {
+                weightedDigits.Add(digits[i] * positionWeights[i]);
+            }
+            int sum = weightedDigits.Sum();
+
+            if (sum % 89 == 0) // abn is Validate if sum-of-weighted-digits mod 89 is zero
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+
+namespace Tests
+{
+    public class Tests
+    {
+        [TestCase("30608629947")]                 // BHP
+        [TestCase("30 608 629 947")]              // BHP - standard masked format
+        [TestCase("306 08 62 99 47")]             // BHP - non-standard format
+        [TestCase("   3 0 6 0 8 6 2 9 9 4 7  ")]  // BHP - extra spaces
+
+        [TestCase("51 824 753 556")]              // AUSTRALIAN TAX OFFICE
+        [TestCase("98 008 624 691")]              // ASX Limited
+        public void TestAbnValid(string abn)
+        {
+            Assert.True(AbnValidator.Validate(abn));
+        }
+
+
+        [TestCase("")]
+        [TestCase("1")]
+        [TestCase("12345678910")]
+        [TestCase("abcxyz")]
+        [TestCase("30608628947")]   // BHP - one digit incorrect
+        public void TestAbnsInvalid(string abn)
+        {
+            Assert.False(AbnValidator.Validate(abn));
+        }
+    }
+}
