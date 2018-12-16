697190b62ea73b7b00af05c14241b625de48a05d
mbishop8 <mbishop8@myune.edu.au>
2018-12-16 18:54:39 +1100

    Added unit test project

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,31 +1,18 @@
 using NUnit.Framework;
 
-
 namespace Tests
 {
     public class Tests
     {
-        [TestCase("30608629947")]                 // BHP
-        [TestCase("30 608 629 947")]              // BHP - standard masked format
-        [TestCase("306 08 62 99 47")]             // BHP - non-standard format
-        [TestCase("   3 0 6 0 8 6 2 9 9 4 7  ")]  // BHP - extra spaces
-
-        [TestCase("51 824 753 556")]              // AUSTRALIAN TAX OFFICE
-        [TestCase("98 008 624 691")]              // ASX Limited
-        public void TestAbnValid(string abn)
+        [SetUp]
+        public void Setup()
         {
-            Assert.True(AbnValidator.Validate(abn));
         }
 
-
-        [TestCase("")]
-        [TestCase("1")]
-        [TestCase("12345678910")]
-        [TestCase("abcxyz")]
-        [TestCase("30608628947")]   // BHP - one digit incorrect
-        public void TestAbnsInvalid(string abn)
+        [Test]
+        public void Test1()
         {
-            Assert.False(AbnValidator.Validate(abn));
+            Assert.Pass();
         }
     }
 }
